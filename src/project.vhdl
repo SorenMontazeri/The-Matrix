82c5e45d7de2cad8f63774f823172424a8361e01
sorho210 <sorho210@muxen2-108.ad.liu.se>
2025-11-08 17:31:47 +0100

    imma kms

--- src/project.vhdl
+++ src/project.vhdl
@@ -2,7 +2,7 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
-entity tt_um_example is
+entity tt_um_matrix is
     port (
         ui_in   : in  std_logic_vector(7 downto 0);  -- IN pins
         uo_out  : out std_logic_vector(7 downto 0);  -- OUT pins
